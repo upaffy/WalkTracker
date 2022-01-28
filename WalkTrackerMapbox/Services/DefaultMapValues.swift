9d0fa2a4235304726c6be6dfe5b6376d33653b81
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-01-28 22:35:34 +0300

    remove extra location from default values

--- WalkTrackerMapbox/Services/DefaultMapValues.swift
+++ WalkTrackerMapbox/Services/DefaultMapValues.swift
@@ -8,6 +8,5 @@
 import MapboxMaps
 
 enum DefaultMapValues {
-    static let locationCL = CLLocationCoordinate2D(latitude: 59.956165, longitude: 30.309382)
-    static let location = Location(with: CLLocation(latitude: locationCL.latitude, longitude: locationCL.longitude))
+    static let location = Location(with: CLLocation(latitude: 59.956165, longitude: 30.309382))
 }
