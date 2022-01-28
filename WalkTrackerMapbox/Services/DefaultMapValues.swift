1c1df1e1689c24672e5384933d10051863cffdea
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-01-28 20:58:43 +0300

    Default locations have been added

--- WalkTrackerMapbox/Services/DefaultMapValues.swift
+++ WalkTrackerMapbox/Services/DefaultMapValues.swift
@@ -5,4 +5,9 @@
 //  Created by Pavlentiy on 28.01.2022.
 //
 
-import Foundation
+import MapboxMaps
+
+enum DefaultMapValues {
+    static let locationCL = CLLocationCoordinate2D(latitude: 59.956165, longitude: 30.309382)
+    static let location = Location(with: CLLocation(latitude: locationCL.latitude, longitude: locationCL.longitude))
+}
